21250ab1e57ecae45ef109216ef14e3fb6b12966
Charletas576 <alvaro0711sanchez@gmail.com>
2026-03-24 19:21:59 +0100

    Update factory_main.java

--- factory_main.java
+++ factory_main.java
@@ -12,9 +12,13 @@ public class factory_main {
         System.out.println(AlmacenReal.buscarTrabajador("12345678A"));
 
         GestionFabricaReal.anadirGasolina("M-GAS-01", 1.6, 110, 4);
+        System.out.println(AlmacenReal.buscarMotor("M-GAS-01"));
+
         
         GestionFabricaReal.anadirTela("T-AZUL-01", "Azul", 5);
+        System.out.println(AlmacenReal.buscarTapiceria("T-AZUL-01"));
         
         GestionFabricaReal.anadirNormal("R-STD-01", 205, 16, 500, 210);
+        System.out.println(AlmacenReal.buscarRueda("R-STD-01"));
     }
 }
