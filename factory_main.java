cc3c0acc770e283e5613df9e5c003eca6cc0ec20
Charletas576 <alvaro0711sanchez@gmail.com>
2026-03-24 19:20:03 +0100

    Update factory_main.java

--- factory_main.java
+++ factory_main.java
@@ -10,5 +10,11 @@ public class factory_main {
         GestionFabricaReal.altaAdmin("Paco", "Perez", "12345678A", "Calle A", 11111111L, 1200, new Date());
 
         System.out.println(AlmacenReal.buscarTrabajador("12345678A"));
+
+        GestionFabricaReal.anadirGasolina("M-GAS-01", 1.6, 110, 4);
+        
+        GestionFabricaReal.anadirTela("T-AZUL-01", "Azul", 5);
+        
+        GestionFabricaReal.anadirNormal("R-STD-01", 205, 16, 500, 210);
     }
 }
