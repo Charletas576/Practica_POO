9b56d4f9186e5a711204f469430ec3015d0a1fe8
Charletas576 <alvaro0711sanchez@gmail.com>
2026-03-24 19:10:56 +0100

    Update factory_main.java

--- factory_main.java
+++ factory_main.java
@@ -1,5 +1,6 @@
 public class factory_main {
     public static void main(String[] args) {
-        
+        Almacen AlmacenReal = new Almacen();
+        GestionFabrica GestionFabricaReal = new GestionFabrica();
     }
 }
