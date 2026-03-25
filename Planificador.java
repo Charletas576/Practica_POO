36842326c3282f5f44bed25751fc0d851746d56f
Charletas576 <alvaro0711sanchez@gmail.com>
2026-03-25 18:51:31 +0100

    Update Planificador.java

--- Planificador.java
+++ Planificador.java
@@ -1,18 +1,22 @@
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Planificador {
 
     private Almacen miAlmacen;
+    private MonTurismo miMontaje;
     private ArrayList<Operario> operarios = new ArrayList<>();
 
-    public Planificador(Almacen miAlmacen, TipoMontaje miMontaje) {
-        this.miAlmacen = miAlmacen;
+    public Planificador(Vehiculo vActual, TipoMotor mActual, TipoTapiceria taActual, ArrayList<TipoRueda> rActuales) {
+        miAlmacen = new Almacen();
+        miMontaje = new MonTurismo(vActual, mActual, taActual, rActuales);
     }
 
-    public void simulacion(Vehiculo miVehiculo) {
-        setOperarios(miAlmacen.listarTrabajadores());
-        miMontaje.setVActual(miVehiculo);
+    public void simulacion(String color, int nPlazas, float tara, int pma, String id) {
+        Turismo miTurismo = new Turismo(color, nPlazas, tara, pma, id);
+        setOperarios();
+        miMontaje.setVActual(miTurismo);
         miMontaje.asignarOperarios(operarios);
         int segundosTotales = 0;
         while (miMontaje.getFActual() < 4) {
@@ -22,15 +26,18 @@ public class Planificador {
 
         System.out.println("Simulación finalizada. Tiempo total de montaje: " + segundosTotales + " segundos.");
 
-        miAlmacen.nuevoVehiculo(miVehiculo);
+        miAlmacen.nuevoVehiculo(miTurismo);
     }
 
-    public void setOperarios(ArrayList<Trabajador> lTrabajadores) {
-        while (operarios.size() < 4) {
-            // Saco la posición aleatoria entre los trabajadores operarios
-            Math.random();
-            // Reviso si la posición está ya en el array de operarios
-            // Si no lo está, lo añado
-        }
+    public void setOperarios() {
+        Operario op1 = new Operario("Juan", "García", "12345678A", "Calle A, 1", 100000001L, "Operario", 1200, new Date(), (int) (Math.random() * 21));
+        Operario op2 = new Operario("Ana", "López", "87654321B", "Calle B, 2", 200000002L, "Operario", 1200, new Date(), (int) (Math.random() * 21));
+        Operario op3 = new Operario("Pedro", "Pérez", "11223344C", "Calle C, 3", 300000003L, "Operario", 1200, new Date(), (int) (Math.random() * 21));
+        Operario op4 = new Operario("Lucía", "Sanz", "44332211D", "Calle D, 4", 400000004L, "Operario", 1200, new Date(), (int) (Math.random() * 21));
+
+        operarios.add(op1);
+        operarios.add(op2);
+        operarios.add(op3);
+        operarios.add(op4);
     }
 }
